59805c4769f2231fa7dfa31e18609d8b06965178
Kolappan, Inderjit <jeeth7@gmail.com>
2017-07-10 19:04:03 +0530

    new updates

---
+++ MachineLearning.R
@@ -0,0 +1,17 @@
+#Machine learning with R
+
+data("iris")
+print(iris)
+
+x<-c(1,2,3,4,5)
+y=c(3,4,5)  
+x+y
+x=c(x,9)
+
+test.tdata<- 10
+test.hodssa<-100
+test.hodssa+test.tdata
+assign("test.Jas",50)
+test.Jas
+varName <-"test.Jas"
+assign(varName,100)
